f2f7ad263f021885c75ae067a7c72b9d615e6d1f
Ricardo Delfin <me@rdelfin.com>
2024-06-05 23:18:06 +0100

    Added basic examples

---
+++ examples/simple_pub.rs
@@ -0,0 +1,16 @@
+use robotica::Node;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let node = Node::new("simple_pub".to_string()).await?;
+    let publisher = node.publish("test_topic".to_string()).await?;
+    let mut i = 0;
+    loop {
+        let msg = format!("Hello, World! {}", i);
+        println!("Sending: \"{}\"", msg);
+        publisher.send(msg).await?;
+        i += 1;
+        tokio::time::sleep(Duration::from_millis(500)).await;
+    }
+}
---
+++ examples/simple_sub.rs
@@ -0,0 +1,11 @@
+use robotica::Node;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let node = Node::new("simple_sub".to_string()).await?;
+    let subscriber = node.subscribe("test_topic".to_string()).await?;
+    while let Ok(value) = subscriber.recv().await {
+        println!("Received: \"{}\"", value);
+    }
+    Ok(())
+}
